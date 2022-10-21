c4afa2371d3cac2e562bd3ad501caec4603eec18
bhalla <bhalla@ncbs.res.in>
2022-10-21 14:39:47 +0530

    Further checks included in hossCheck.py

--- hossCheck.py
+++ hossCheck.py
@@ -35,7 +35,7 @@ optimization will succeed.
 * Hoss config file checks so that specified hierarchy levels are in sequence
 * File requirements tests to ensure that all the referred files are present
 * Expt File correctness tests using Findsim-Schema.json
-- Field existence tests to ensure that all altered entities/parameters exist
+* Field existence tests to ensure that all altered entities/parameters exist
 - Optimization sanity tests to ensure that we don't try to optimize too
     many parameters with too few experiments
 '''
@@ -47,26 +47,27 @@ import os
 import time
 import multi_param_minimization
 
-ScorePow = 2.0
-
 HOSS_SCHEMA = "hossSchema.json"
 FINDSIM_SCHEMA = "FindSim-Schema.json"
+HTobjFields = ["concInit", "KA", "tau", "tau2", "Kmod", "Amod", "gain", "baseline" ]
+ODEobjFields = ["concInit", "Kd", "tau", "Kf", "Kb", "Km", "kcat"]
 
 ######################################
 
-def main():
-    t0 = time.time()
-    parser = argparse.ArgumentParser( description = 
-            'This script does a sweep of tests to catch cases that may prevent an optimization from working. ')
-    parser.add_argument( 'config', type = str, help='Required: JSON configuration file for doing the optimization.')
-    parser.add_argument( '-m', '--model', type = str, help='Optional: Composite model definition file. First searched in directory "location", then in current directory.' )
-    parser.add_argument( '-map', '--map', type = str, help='Model entity mapping file. This is a JSON file.' )
-    parser.add_argument( '-e', '--exptDir', type = str, help='Optional: Location of experiment files.', default = "./Expts" )
-    parser.add_argument( '-o', '--optfile', type = str, help='Optional: File name for saving optimized model', default = "" )
-    parser.add_argument( '-r', '--resultfile', type = str, help='Optional: File name for saving results of optimizations as a table of scale factors and scores.', default = "" )
-    args = parser.parse_args()
+def findHTmols( model ):
+    ret = []
+    for gg in model["Groups"].values():
+        if "Eqns" in gg:
+            ret.extend( [ee for ee in gg["Eqns"]] )
+        if "Reacs" in gg:
+            ret.extend( [rr for rr in gg["Reacs"]] )
+        if "Species" in gg:
+            ret.extend( [ss for ss in gg["Species"]] )
+    if "Constants" in model: # We also permit scaling of named constts.
+        ret.extend( [cc for cc in model["Constants"]] )
+    return ret
 
-    # Load and validate the config file
+def validateConfig( args ):
     try:
         with open( args.config ) as json_file:
             config = json.load( json_file )
@@ -85,12 +86,77 @@ def main():
         quit()
 
     try:
-        with open( FINDSIM_SCHEMA ) as fsSchemaFile:
+        if relpath != '': # Go look in Hoss directory
+            fs = relpath + '/' + FINDSIM_SCHEMA
+        else:
+            fs = FINDSIM_SCHEMA
+        with open( fs ) as fsSchemaFile:
             fsSchema = json.load( fsSchemaFile )
             #fsSchemaValidator = jsonschema.protocols.Validator( fsSchema )
     except IOError:
-        print( "Error: Unable to find FindSime schema file: " + FINDSIM_SCHEMA )
+        print( "Error: Unable to find FindSim schema file: " + FINDSIM_SCHEMA )
         quit()
+    print( "Validated config file: ", args.config )
+    return config, fsSchema 
+
+def main():
+    t0 = time.time()
+    parser = argparse.ArgumentParser( description = 
+            'This script does a sweep of tests to catch cases that may prevent an optimization from working. ')
+    parser.add_argument( 'config', type = str, help='Required: JSON configuration file for doing the optimization.')
+    parser.add_argument( '-m', '--model', type = str, help='Optional: Composite model definition file. First searched in directory "location", then in current directory.' )
+    parser.add_argument( '-map', '--map', type = str, help='Model entity mapping file. This is a JSON file.' )
+    parser.add_argument( '-e', '--exptDir', type = str, help='Optional: Location of experiment files.', default = "./Expts" )
+    parser.add_argument( '-o', '--optfile', type = str, help='Optional: File name for saving optimized model', default = "" )
+    parser.add_argument( '-r', '--resultfile', type = str, help='Optional: File name for saving results of optimizations as a table of scale factors and scores.', default = "" )
+    args = parser.parse_args()
+
+    ## Check 0: Load and validate the config file
+    config, fsSchema = validateConfig( args )
+
+
+    ## Check 0.1: Load and validate the map file. We'll use it later too.
+    if args.map == None:
+        mapFileName = config["map"]
+    else:
+        mapFileName = args.map
+    try:
+        with open( mapFileName ) as map_file:
+            try:
+                modelmap = json.load( map_file )
+            except json.decoder.JSONDecodeError as err:
+                print( "Error: Load/syntax error in map file: '{}'".format( mapFileName ) )
+                print( err )
+                quit()
+    except IOError:
+        print( "Error: Unable to find map file: " + mapFileName )
+        quit()
+    print( "Validated map file: ", mapFileName )
+
+    ## Check 0.2: Load and validate the model file. We'll use it later too.
+    if args.model == None:
+        modelFileName = config["model"]
+    else:
+        modelFileName = args.model
+    try:
+        with open( modelFileName ) as model_file:
+            modelSuffix = modelFileName.split( "." )[-1]
+            if modelSuffix == "json":
+                try:
+                    model = json.load( model_file )
+                    modelMols = findHTmols( model )
+                except json.decoder.JSONDecodeError as err:
+                    print( "Error: Load/syntax error in model file: '{}'".format( modelFileName ) )
+                    print( err )
+                    quit()
+                objFields = HTobjFields
+            else:
+                objFields = ODEobjFields
+                modelMols = []
+    except IOError:
+        print( "Error: Unable to find model file: " + modelFileName )
+        quit()
+    print( "Validated model file: ", modelFileName )
 
     blocks = config["HOSS"]
     fail = False
@@ -125,6 +191,7 @@ def main():
 
     ## Check 3: All expt files are well-formed JSON and pass the FindSim-Schema
     numExpts = 0
+    exptDefnDict = {}
     for idx, bb in enumerate( blocks ):
         for pname, pblock in bb.items():
             if not pname in ["name", "hierarchyLevel"]:
@@ -157,6 +224,38 @@ def main():
         print( "\n" )
         quit()
     print( "\nValidated all {} named experiment files.".format( numExpts ) )
+
+    ## Check 4: All named objects exist in the map file.
+    numParams = 0
+    for idx, bb in enumerate( blocks ):
+        for pname, pblock in bb.items():
+            if not pname in ["name", "hierarchyLevel"]:
+                params = pblock['params']
+                for pp in params:
+                    ppsp = pp.split(".")
+                    if len( ppsp ) != 2:
+                        print( "\nError: Bad param formatting for '{}' in pathway '{}' in block {}".format( pp, pname, idx+1 ) )
+                        fail = True
+                    obj = ppsp[0]
+                    field = ppsp[1]
+                    ok = True
+                    if not field in objFields:
+                        print( "\nError: Unknown field name: '{}' on obj '{}' in pathway '{}' in block {}".format( field, obj, pname, idx+1 ) )
+                        fail = True
+                        ok = False
+                    if (obj in modelmap) or (obj in modelMols):
+                        if ok:
+                            print( ".", end = "", flush=True)
+                            numParams += 1
+                    else:
+                        print( "\nError: Missing Obj name in mapfile: '{}' in pathway '{}' in block {}".format( obj, pname, idx+1 ) )
+                        fail = True
+    if fail: 
+        print( "\n" )
+        quit()
+    print( "\nValidated all {} parameters.".format( numParams ) )
+
+
         
 # Run the 'main' if this script is executed standalone.
 if __name__ == '__main__':
